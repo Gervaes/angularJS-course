184fcce7c603f6db4ea16765110ce21636952e2c
Gervaes <guilherme.gervaes@rotadasoficinas.com.br>
2022-03-09 10:05:38 -0300

    Last Lessons

---
+++ Lessons/Lesson38/app.js
@@ -0,0 +1,69 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    
+    $routeProvider
+    
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'mainController'
+    })
+    
+    .when('/second', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+    .when('/second/:num', {
+        templateUrl: 'pages/second.html',
+        controller: 'secondController'
+    })
+    
+});
+
+myApp.controller('mainController', ['$scope', '$log', function($scope, $log) {
+    
+    $scope.people = [{
+            name: 'John Doe',
+            address: '555 Main St.',
+            city: 'New York',
+            state: 'NY',
+            zip: '11111', 
+        },{
+            name: 'Jane Doe',
+            address: '222 Second St.',
+            city: 'Buffalo',
+            state: 'NY',
+            zip: '222222', 
+        },{
+            name: 'George Doe',
+            address: '111 Third St.',
+            city: 'Miami',
+            state: 'FL',
+            zip: '333333', 
+        },
+    ];
+
+    $scope.formattedAddress = function(person) {
+        return person.address + ', ' + person.city + ', ' + person.state + ', ' + person.zip;
+    }
+    
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function($scope, $log, $routeParams) {
+    
+    
+    
+}]);
+
+myApp.directive("searchResult", function() {
+   return {
+       restrict: 'AECM',
+       templateUrl: 'directives/searchresult.html',
+       replace: true,
+       scope: {
+           personObject: "=", //two-way binding, can pass objects
+           formattedAddressFunction: "&" //function
+       }
+   }
+});
